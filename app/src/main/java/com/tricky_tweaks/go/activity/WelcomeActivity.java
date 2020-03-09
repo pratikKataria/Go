f9631a4017ff505f815bf89e3d1c0927481bcf6c
pratik <pratikkatariya3.pk@gmail.com>
2020-03-09 11:34:33 -0700

    Welcome Activity - image button click implemented

--- app/src/main/java/com/tricky_tweaks/go/activity/WelcomeActivity.java
+++ app/src/main/java/com/tricky_tweaks/go/activity/WelcomeActivity.java
@@ -1,7 +1,12 @@
 package com.tricky_tweaks.go.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import com.tricky_tweaks.go.R;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -10,5 +15,13 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        ImageButton s = findViewById(R.id.image_btn);
+        s.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+            }
+        });
     }
 }
