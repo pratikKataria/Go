23a17ccc796f7a024c3cc8b4f8f75d8f058fb479
pratik <pratikkatariya3.pk@gmail.com>
2020-03-09 07:24:53 -0700

    WelcomeActivity - new create

--- app/src/main/java/com/tricky_tweaks/go/activity/WelcomeActivity.java
+++ app/src/main/java/com/tricky_tweaks/go/activity/WelcomeActivity.java
@@ -1,14 +1,14 @@
-package com.tricky_tweaks.go;
+package com.tricky_tweaks.go.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.tricky_tweaks.go.R;
 
-public class MainActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
     }
 }
