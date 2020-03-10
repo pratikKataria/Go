a0710ceeb96dca9ae9c7424097ba3efc3de1f9c4
pratik <pratikkatariya3.pk@gmail.com>
2020-03-10 00:09:37 -0700

    DetailFormFragment - ui created

---
+++ app/src/main/java/com/tricky_tweaks/go/LoginBundle/fragment/DetailForm.java
@@ -0,0 +1,36 @@
+package com.tricky_tweaks.go.LoginBundle.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tricky_tweaks.go.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetailForm extends Fragment {
+
+
+    public DetailForm() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_detail_form, container, false);
+
+
+
+        return view;
+    }
+
+}
