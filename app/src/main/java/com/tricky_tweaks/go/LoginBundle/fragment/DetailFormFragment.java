7b07de144f3b1c52866efff68a4bba9ebfcca42c
pratik <pratikkatariya3.pk@gmail.com>
2020-03-10 10:26:09 -0700

    DetailFormFragment - progressbar implemented and realtime database data push test code working fine

--- app/src/main/java/com/tricky_tweaks/go/LoginBundle/fragment/DetailFormFragment.java
+++ app/src/main/java/com/tricky_tweaks/go/LoginBundle/fragment/DetailFormFragment.java
@@ -5,17 +5,46 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.tricky_tweaks.go.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DetailFormFragment extends Fragment {
 
+    EditText userName;
+    RadioGroup gender;
+    EditText roomNo;
+    EditText userPhoneNo;
+    EditText fatherName;
+    EditText fatherPhoneNumber;
+    EditText address;
+    MaterialButton saveButton;
+
+    private void init_fields(View v) {
+        userName = v.findViewById(R.id.fragment_detail_form_et_username);
+        roomNo = v.findViewById(R.id.fragment_detail_form_et_room_no);
+        userPhoneNo = v.findViewById(R.id.fragment_detail_form_et_phone_no);
+        fatherName = v.findViewById(R.id.fragment_detail_form_et_father_name);
+        fatherPhoneNumber = v.findViewById(R.id.fragment_detail_form_et_father_phone_no);
+        address = v.findViewById(R.id.fragment_detail_form_et_address);
+
+        saveButton = v.findViewById(R.id.fragment_detail_form_mb_save);
+    }
 
     public DetailFormFragment() {
         // Required empty public constructor
@@ -28,8 +57,17 @@ public class DetailFormFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_detail_form, container, false);
 
+        init_fields(view);
 
+        saveButton.setOnClickListener(v -> {
+            Map<String, Object> map = new HashMap<>();
+            map.put("s_name", userName.getText().toString());
+            map.put("s_phoneNo", userPhoneNo.getText().toString());
 
+            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Student/"+FirebaseAuth.getInstance().getUid());
+
+            databaseReference.updateChildren(map);
+        });
         return view;
     }
 
