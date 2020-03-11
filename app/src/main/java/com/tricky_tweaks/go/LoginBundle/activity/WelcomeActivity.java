060544787578d1f4cc8eb471835d44aa08831a34
pratik <pratikkatariya3.pk@gmail.com>
2020-03-10 20:30:04 -0700

    WelcomeActivity - close and next Button func implemented

--- app/src/main/java/com/tricky_tweaks/go/LoginBundle/activity/WelcomeActivity.java
+++ app/src/main/java/com/tricky_tweaks/go/LoginBundle/activity/WelcomeActivity.java
@@ -4,19 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageButton;
 
 import com.tricky_tweaks.go.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private ImageButton closeBtn;
+    private ImageButton nextBtn;
+
+    private void init_fields() {
+        closeBtn = findViewById(R.id.activity_welcome_ib_close);
+        nextBtn = findViewById(R.id.activity_welcome_ib_next);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        ImageButton s = findViewById(R.id.image_btn);
-        s.setOnClickListener(v -> startActivity(new Intent(WelcomeActivity.this, EntryActivity.class)));
+        init_fields();
+
+        closeBtn.setOnClickListener(v -> finish());
+        nextBtn.setOnClickListener(v -> {
+            startActivity(new Intent(WelcomeActivity.this, EntryActivity.class));
+            finish();
+        });
+
     }
 }
