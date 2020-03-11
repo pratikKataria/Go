ecda1789876c633d55abd54efa5c6b3f557cbb38
pratik <pratikkatariya3.pk@gmail.com>
2020-03-11 11:48:12 -0700

    General - GatePassActivity created

---
+++ app/src/main/java/com/tricky_tweaks/go/MainBundle/activity/GatePassActivity.java
@@ -0,0 +1,16 @@
+package com.tricky_tweaks.go.MainBundle.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tricky_tweaks.go.R;
+
+public class GatePassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gate_pass);
+    }
+}
