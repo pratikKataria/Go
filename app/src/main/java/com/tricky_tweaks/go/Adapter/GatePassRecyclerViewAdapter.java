89aca7097d0a5084a6e67bc3ce8345bbf2b1277f
Castell <pratikkatariya3.pk@gmail.com>
2020-03-15 19:09:49 -0500

    GatePassRecyclerViewAdapter - request accept button added

--- app/src/main/java/com/tricky_tweaks/go/Adapter/GatePassRecyclerViewAdapter.java
+++ app/src/main/java/com/tricky_tweaks/go/Adapter/GatePassRecyclerViewAdapter.java
@@ -1,11 +1,23 @@
 package com.tricky_tweaks.go.Adapter;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.ColorStateList;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.tricky_tweaks.go.DataModel.GatePassData;
 import com.tricky_tweaks.go.R;
 
@@ -39,6 +51,56 @@ public class GatePassRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerVi
             p.textViewModerator.setText(list.get(position).getGp_moderator());
             p.textViewTime.setText(list.get(position).getGp_time());
             p.textViewUID.setText(list.get(position).getS_id());
+
+            if (list.get(position).getGp_status() == 1) {
+                p.textViewstatus.setText("accepted");
+                p.textViewstatus.setTextColor(context.getColor(R.color.green));
+            } else {
+                p.textViewstatus.setText("pending");
+                p.textViewstatus.setTextColor(context.getColor(R.color.ceriseRed));
+            }
+
+            p.requestButton.setOnClickListener(n -> {
+                showAlertDialog(position);
+                Toast.makeText(context, "CLICKED", Toast.LENGTH_SHORT).show();
+            });
+    }
+
+    public void showAlertDialog(int position) {
+        new MaterialAlertDialogBuilder(context, R.style.AlertDialogTheme)
+                .setTitle("Request")
+                .setMessage("Would you like to accept the request ")
+                .setCancelable(false)
+                .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("GatePass");
+                        reference.child(list.get(position).getGp_id()).child("gp_status").setValue(1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(context, "successfull", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(context, "failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(context, "error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                        Toast.makeText(context, "yes", Toast.LENGTH_SHORT).show();
+                    }
+                }).setNeutralButton("no", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Toast.makeText(context, "no", Toast.LENGTH_SHORT).show();
+            }
+        }).show();
     }
 
     @Override
@@ -55,6 +117,8 @@ public class GatePassRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerVi
         TextView textViewModerator;
         TextView textViewTime;
         TextView textViewDate;
+        TextView textViewstatus;
+        Chip requestButton;        
 
         public PassViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -63,6 +127,9 @@ public class GatePassRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerVi
             textViewTo = itemView.findViewById(R.id.card_view_gp_to);
             textViewModerator = itemView.findViewById(R.id.card_view_gp_moderator);
             textViewTime = itemView.findViewById(R.id.card_view_gp_time);
+        
+            textViewstatus = itemView.findViewById(R.id.card_view_gp_status);
+            requestButton = itemView.findViewById(R.id.card_view_gp_chip_request);
         }
     }
 }
