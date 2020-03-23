883e44f810a678827cb1497d8cbc3b4a206cccae
Castell <pratikkatariya3.pk@gmail.com>
2020-03-23 04:44:26 -0500

    CreateGatePassActivity - getSlatString removed

--- app/src/main/java/com/tricky_tweaks/go/MainBundle/activity/CreateGatePassActivity.java
+++ app/src/main/java/com/tricky_tweaks/go/MainBundle/activity/CreateGatePassActivity.java
@@ -239,16 +239,4 @@ public class CreateGatePassActivity extends AppCompatActivity {
         }
     }
 
-    protected String getSaltString() {
-        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-        StringBuilder salt = new StringBuilder();
-        Random rnd = new Random();
-        while (salt.length() < 18) { // length of the random string.
-            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
-            salt.append(SALTCHARS.charAt(index));
-        }
-        String saltStr = salt.toString();
-        return saltStr;
-
-    }
 }
